d2733b25a9041737e105f673aa3db22b2194da1f
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-01-25 12:54:18 +0530

    Day2

--- count_digits.py
+++ count_digits.py
@@ -1,3 +1,5 @@
+# Brute force
+
 num = int(input("Enter a number: "))
 a = num
 count = 0
@@ -5,3 +7,9 @@ while a > 0:
     count += 1
     a = a//10
 print(count)
+
+# Optimal
+
+from math import log10, ceil
+
+print(ceil(log10(num)))
