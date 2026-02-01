1e5f3a6757c1e6a170d7409caa395c0bd51e37d5
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-01 17:45:31 +0530

    Day5

--- highest_lowest_freq_2.py
+++ highest_lowest_freq_2.py
@@ -1,30 +1,21 @@
 def highest_lowest_freq(l,k):
-    d = {}
     l.sort()
-
-    s = 0
-    e = len(l)-1
-    m = s + (e-s)//2
     ans = 1
 
-    while s < e:
+    for i in range(len(l)):
         a = 0
-        for i in range(m,0,-1):
-            a += l[m]-l[i-1]
-            if a > k:
-                e = m
-                m = s + (e-s)//2
-                ans = max(ans,m-i+1)
+        j = 0
+        while (i - j) >= 0:
+            cost = l[i] - l[i-j]
+            if a + cost > k:
+                break
+            a += cost
+            j += 1
+            
+        ans = max(ans, j)
         
-                
-
-    for i in l:
-        if i in d.keys():
-            d[i] += 1
-        else:
-            d[i] = 1
-    
-    return max(d), min(d)
+    return ans
+                    
 
 l = [1,2,4]
 k = 5
