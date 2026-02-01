b7051fbed6b124a2d1a0d908de82741439cf43a1
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-01 18:43:26 +0530

    Day5

--- highest_lowest_freq_2.py
+++ highest_lowest_freq_2.py
@@ -1,3 +1,5 @@
+# Brute force
+
 def highest_lowest_freq(l,k):
     l.sort()
     ans = 1
@@ -20,3 +22,34 @@ def highest_lowest_freq(l,k):
 l = [1,2,4]
 k = 5
 print(highest_lowest_freq(l,k))
+
+
+# Optimal (Sliding Window)
+
+def highest_lowest_freq_optimized(nums, k):
+    nums.sort()
+    left = 0
+    total = 0
+    ans = 0
+    
+    # 'right' expands the window
+    for right in range(len(nums)):
+        # Add the new number to our window sum
+        total += nums[right]
+        
+        # LOGIC:
+        # We want to make all numbers in the window [left...right] equal to nums[right].
+        # The cost is: (target * count) - (current_sum)
+        # Cost = (nums[right] * window_length) - total
+        
+        while (nums[right] * (right - left + 1)) - total > k:
+            # If cost is too high, shrink window from the left
+            total -= nums[left]
+            left += 1
+            
+        # Update max window size found so far
+        ans = max(ans, right - left + 1)
+        
+    return ans
+
+print(highest_lowest_freq_optimized(l, k))
