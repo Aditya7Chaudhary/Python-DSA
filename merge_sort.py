92fe18169cca4ada129195ef4943fb9c8f49a8fa
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-02 14:03:57 +0530

    Day6

--- merge_sort.py
+++ merge_sort.py
@@ -1,31 +1,37 @@
-def merge(l,s,m,e):
-    l_ans = []
-    s1 = 0
-    s2 = 0
-    for i in range(e):
-        if len(l[s]) == s1:
-            l_ans.extend(l2)
-            break
-        if len(l2) == s2:
-            l_ans.extend(l1)
-            break
+class Solution:
+    def merge(self,l,s,m,e):
+        temp = []
+        left = s
+        right  = m+1
 
-        if l1[s1] >= l2[s2]:
-            l_ans.append(l2[s2])
-            s2 += 1            
-        else:
-            l_ans.append(l1[s1])
-            s1 += 1
-    
-    return l_ans
+        while left <= m and right <= e:
+            if l[left] <= l[right]:
+                temp.append(l[left])
+                left += 1
+            else:
+                temp.append(l[right])
+                right += 1
 
-def merge_sort(l,s,e):
-    if s > e:
-        return 
-    m = s + (e-s)//2
-    merge_sort(l,s,m)
-    merge_sort(l,m+1,e)
-    merge(l,s,m,e)
+        while left <= m:
+            temp.append(l[left])
+            left += 1
+        while right <= e:
+            temp.append(l[right])
+            right += 1
+
+        for i in range(s,e+1):
+            l[i] = temp[i-s]
+        
+
+    def merge_sort(self,l,s,e):
+        if s >= e:
+            return 
+        m = s + (e-s)//2
+        self.merge_sort(l,s,m)
+        self.merge_sort(l,m+1,e)
+        self.merge(l,s,m,e)
     
-l = [3,1,2,4,1,5,2,6,4]
-print(merge_sort(l,0,len(l)-1))
+l = [3,2,4,1,5,2,6,4]
+sol = Solution()
+sol.merge_sort(l, 0, len(l) - 1)
+print(l)
