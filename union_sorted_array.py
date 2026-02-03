622866b88ae41239b7c913b3543ca5c8aa668c43
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-03 17:07:32 +0530

    Day7

--- union_sorted_array.py
+++ union_sorted_array.py
@@ -1,30 +1,36 @@
 def union_sorted_array(l1,l2):
-    i = 0
-    j = 0
-    l = []
+    i = 1
+    j = 1
+    if l1[0] < l2[0]:
+        l = [l1[0],l2[0]]
+    elif l2[0] < l1[0]:
+        l = [l2[0],l1[0]]
+    else:
+        l = [l1[0]]
+
     while i < len(l1) and j < len(l2):
-        if l1[i] not in l and l2[j] not in l:
+        if l1[i] != l[-1] and l2[j] != l[-1]:
             if l1[i] <= l2[j]:
                 l.append(l1[i])
                 i += 1
             else:
                 l.append(l2[j])
                 j += 1
-        elif l1[i] in l and l2[j] in l:
+        elif l1[i] == l[-1] and l2[j] == l[-1]:
             i += 1
             j += 1
-        elif l2[j] in l:
+        elif l2[j] == l[-1]:
             j += 1
         else:
             i += 1
     
     if i >= len(l1):
         for a in range(j,len(l2)):
-            if l2[a] not in l:
+            if l2[a] != l[-1]:
                 l.append(l2[a])
     elif j >= len(l2):
         for a in range(i,len(l1)):
-            if l1[a] not in l:
+            if l1[a] != l[-1]:
                 l.append(l1[a])
 
     return l
