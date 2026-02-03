5074e7f922ad26b35e5ae134330101988d0006a9
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-03 11:45:58 +0530

    Day7

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,3 +1,5 @@
+# Brute-force Solution 
+
 def remove_duplicates(l):
     lu = [l[0]]
     for i in range(1,len(l)):
@@ -5,5 +7,21 @@ def remove_duplicates(l):
             lu.append(l[i])
     return lu
 
+# Optimized Solution 
+
+def opt_remove_duplicates(l):
+    if not l:
+        return []
+
+    j = 0
+    for i in range(1, len(l)):
+        if l[i] != l[j]:
+            j += 1
+            l[j] = l[i]
+            
+    return l[:j+1]
+
+
 l = [1,2,2,3,3,5,6,7,7]
 print(remove_duplicates(l))
+print(opt_remove_duplicates(l))
