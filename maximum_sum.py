21875059702cb1c8e6bbbfe03bdb40d1d21fa457
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-05 17:36:16 +0530

    Day9

--- maximum_sum.py
+++ maximum_sum.py
@@ -1,24 +1,12 @@
 def maximum_sum(l):
-    left = 0
-    right = len(l)-1
-
     sum = 0
+
     for i in range(len(l)):
         sum += l[i]
-    max_sum = sum
+        if sum < 0:
+            sum = 0
 
-    while left < right:
-        if l[left] < l[right]:
-            sum -= l[left]
-            left += 1
-        else:
-            sum -= l[right]
-            right -= 1
-        
-        if max_sum < sum:
-            max_sum = sum
+        return sum
 
-    return max_sum
-
-l = [2, 3, 5, -2, 7, -4]  
+l = [100,-200,100]  
 print(maximum_sum(l))
