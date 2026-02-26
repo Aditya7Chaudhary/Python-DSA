dfc9708bbe7eadc9518204252f962e756a568e5e
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-26 19:26:17 +0530

    Day13

--- longest_consequtive_array.py
+++ longest_consequtive_array.py
@@ -1,3 +1,5 @@
+# Good Approach
+
 def longest_consequtive_array(l):
     l.sort()
     count = 1
@@ -9,5 +11,33 @@ def longest_consequtive_array(l):
 
     return count
 
+# Best Approach
+
+def longest_consequtive_array_opt(l):
+    st = set()
+    for i in l:
+        st.add(i)
+    
+    ans = 1
+    min_num_l = []
+    
+    for i in range(len(l)):
+        if l[i]-1 not in st:
+            min_num_l.append(i)
+    
+    for i in min_num_l:
+        count = 1
+        num = l[i]+1
+        while num in st:
+            count += 1
+            num += 1
+        if count > ans:
+            ans = count
+                   
+    return ans
+
+
+
 l = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1]
 print(longest_consequtive_array(l))
+print(longest_consequtive_array_opt(l))
