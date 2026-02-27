db23a0c67c71fdba0aeceac53ba892d7c49dc084
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-27 21:34:06 +0530

    Day14

--- rotate_90_degrees.py
+++ rotate_90_degrees.py
@@ -14,6 +14,9 @@ def rotate_90_degrees(matrix):
 
     return matrix
 
-matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+matrix1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 # Output = [[7, 4, 1], [8, 5, 2], [9, 6, 3]]
-print(rotate_90_degrees(matrix))
+matrix2 = [[0, 1, 1, 2], [2, 0, 3, 1], [4, 5, 0, 5], [5, 6, 7, 0]]
+# Output = [[5, 4, 2, 0], [6, 5, 0, 1], [7, 0, 3, 1], [0, 5, 1, 2]]
+print(rotate_90_degrees(matrix1))
+print(rotate_90_degrees(matrix2))
