cedc911783546891b33100b15534e799156cbde6
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-28 16:17:01 +0530

    Day15

--- pascals_triangle.py
+++ pascals_triangle.py
@@ -13,3 +13,29 @@ def pascals_triangle(k):
 
 k = 6
 print("kth row is :",pascals_triangle(k))
+
+# To only show the Kth row
+
+def kth_pascals_triangle(K):
+    def get_nCr(n,r):
+        res = 1
+        # Math optimization: nCr is perfectly equal to nC(n-r). 
+        # If n=10 and r=8, 10C8 is the same as 10C2. We pick the smaller one to loop less!
+        if r > n - r:
+            r = n - r
+            
+        for i in range(r):
+            res = res * (n - i)
+            res = res // (i + 1) # Use integer division
+            
+        return res
+    
+    l = []
+    
+    for i in range(k+1):
+        l.append(get_nCr(k,i))
+
+    return l
+
+k = 5
+print(kth_pascals_triangle(k))
