9e361394d3487d41c18447317d19219f9bef0bc5
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-02-28 20:46:47 +0530

    Day15

--- longest_subarray_zero.py
+++ longest_subarray_zero.py
@@ -1,25 +1,27 @@
 def longest_subarray_zero(l):
-    max_array = []
-    current_array = []
+    max_array = 0
+    left = 0
+    right = 0
+    current_sum = 0
     sums = {}
     n = len(l)
 
-    for i in range(n):
-        current_array = l[:i+1] 
-        current_sum = sum(current_array)
-
-        if current_sum > 0 and current_sum in sums:
-            current_array = l[sums[current_sum]+1:i+1]
-            current_sum = sum(current_array)
+    for i in range(n): 
+        current_sum += l[i]
 
         if current_sum == 0:
-            if len(max_array) < len(current_array):
-                max_array = current_array
-
-        if current_sum != 0:
+            max_array = i+1
+            right = i+1
+            left = 0
+        elif current_sum != 0 and current_sum in sums:
+            if max_array < i+1 - sums[current_sum]:
+                left = sums[current_sum]+1
+                right = i+1
+                max_array = right - left
+        else:
             sums[current_sum] = i
 
-    return max_array
+    return l[left:right]
 
-l = [9, -3, 3, -1, 6, -5]
+l = [6, -2, 2]
 print(longest_subarray_zero(l))
