721a8263bbc835d801201e4d96036f91a384a9b3
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-02 00:16:04 +0530

    Day16

--- maximum_subarray_product.py
+++ maximum_subarray_product.py
@@ -1,59 +1,21 @@
-def maximum_subarray_product(l):
-    l_0,l_neg = [],[]
-    n = len(l)
-    neg_num = 0
-    for i in range(n):
-        if l[i] < 0:
-            neg_num += 1
-        elif l[i] == 0:
-            l_0.append(i)
-            l_neg.append(neg_num)
-            neg_num = 0
-    l_neg.append(neg_num)
-    l_0.append(n)
-
-    max_product = 0
-    for i in range(len(l_neg)):
-        product = 1
-
-        if i > 0:
-            s = l_0[i-1]
-        else:
-            s = 0
-        e = l_0[i]-1
-
-        for j in range(s,e+1):
-                if i > 0 and j==s:
-                    continue
-                product *= l[j]
-        if l_neg[i]%2 == 0:
-            max_product = max(max_product,product)
-        else:
-            while s <= e:
-                if l[s] < 0 and l[e] < 0:
-                    if l[e] < l[s]:
-                        product /= l[s]
-                        break
-                    else:
-                        product /= l[e]
-                        break
-                elif l[s] < 0:
-                    product /= l[s]
-                    break
-                elif l[e] < 0:
-                    product /= l[e]
-                    break
-                else:
-                    if l[s] < l[e]:
-                        product /= l[s]
-                        s += 1
-                    else:
-                        product /= l[e]
-                        e -= 1
-
-            max_product = max(max_product,product)
+def maximum_subarray_product(nums):
+    if not nums: return 0
     
+    max_product = float('-inf')
+    prefix = 1
+    suffix = 1
+    n = len(nums)
+    
+    for i in range(n):
+        if prefix == 0: prefix = 1
+        if suffix == 0: suffix = 1
+        
+        prefix *= nums[i]
+        suffix *= nums[n - 1 - i]
+
+        max_product = max(max_product, prefix, suffix)
+        
     return max_product
 
-nums = [1,2,-3,0,-4,-5]
-print(maximum_subarray_product(nums))
+nums = [1, 2, -3, 0, -4, -5, 0]
+print(maximum_subarray_product(nums)) 
