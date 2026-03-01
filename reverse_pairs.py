dd62ffa8593a750eb702297b637877ccef133d17
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-01 22:42:29 +0530

    Day16

--- reverse_pairs.py
+++ reverse_pairs.py
@@ -5,19 +5,31 @@ def reverse_pairs(l):
         temp = []
 
         while left <= m and right <= e:
-            if l[left] <= l[right]:
+            if l[right]*2 < l[left]:
+                ans += (m+1 - left)
+                right += 1
+            else:
+                left += 2
+        
+        left = s
+        right = m+1
+
+        while left <= m and right <= e:
+            if l[left] < l[right]:
                 temp.append(l[left])
                 left += 1
+            elif l[left] == l[right]:
+                temp.extend([l[left],l[right]])
+                left += 1
+                right += 1
             else:
                 temp.append(l[right])
                 right += 1
-                if l[right]*2 < l[left]:
-                    ans += (m - left + 1)
 
         if left <= m:
             temp.extend(l[left:m+1])
         elif right <= e:
-            temp.extend(l[right:e])
+            temp.extend(l[right:e+1])
 
         l[s:e+1] = temp
         
@@ -33,10 +45,12 @@ def reverse_pairs(l):
         
         return l, ans
 
-    ans = 0
+    ans = 0 
     s = 0
     e = len(l)-1
-    return divide(l,s,e,ans)
+    l,ans = divide(l,s,e,ans)
+
+    return ans
 
 l = [1,3,2,3,1]
 print(reverse_pairs(l))
