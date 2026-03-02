ba4ae8c495d34bca8b91db39d8b2e2abf4c4670a
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-02 19:45:23 +0530

    Day17

---
+++ search_insert_number.py
@@ -0,0 +1,20 @@
+def search_insert_number(l,k):
+    s = 0
+    e = len(l)-1
+
+    while s <= e:
+        m = s + (e-s)//2
+
+        if l[m] == k:
+            return m
+        elif l[m] < k:
+            s = m+1
+        else:
+            e = m-1
+    
+    l = l[:m] + [k] + l[m:]
+    return l
+
+nums = [1,2,4,7]
+x = 6
+print(search_insert_number(nums,x))
