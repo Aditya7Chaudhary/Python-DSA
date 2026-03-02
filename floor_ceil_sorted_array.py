a2f2bbe6cf921ec695955a0677732e4171c6fc20
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-02 20:10:44 +0530

    Day17

--- floor_ceil_sorted_array.py
+++ floor_ceil_sorted_array.py
@@ -5,13 +5,18 @@ def floor_ceil_sorted_array(l,k):
     while s <= e:
         m = s + (e-s)//2
 
-        if l[m] <= k:
+        if l[m] == k:
+            return l[m],l[m]
+        elif l[m] < k:
             s = m+1
         else:
             e = m-1
 
-    return l[e],l[s]
+    floor_val = l[e] if e >= 0 else None
+    ceil_val = l[s] if s < len(l) else None
 
-nums = [3, 4, 4,5, 7, 8, 10]
-x = 4
+    return floor_val,ceil_val
+
+nums = [3, 4, 4, 5, 7, 8, 10]
+x = 6
 print(floor_ceil_sorted_array(nums,x))
