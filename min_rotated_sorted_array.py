2bfce0d518d9f41f65df0543f8ca4525efc86138
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-03 11:23:59 +0530

    Day18

---
+++ min_rotated_sorted_array.py
@@ -0,0 +1,22 @@
+def min_rotated_sorted_array(l):
+    s = 0
+    e = len(l)-1
+    min_num = l[e]
+
+    while s <= e:
+        m = s + (e-s)//2
+        
+        if l[m] > l[s]:
+            if min_num > l[s]:
+                min_num = l[s]
+            s = m+1
+        else:
+            if min_num > l[m]:
+                min_num = l[m]
+            e = m-1
+
+    return min_num
+
+nums = [3,4,5,1,2]
+print(min_rotated_sorted_array(nums))
+        
