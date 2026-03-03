787303a2946d6a3e9e6006096e68965db7613ebc
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-03 11:37:37 +0530

    Day18

---
+++ number_times_rotated.py
@@ -0,0 +1,19 @@
+def number_times_sorted_array_rotated(l):
+    s = 0
+    e = len(l)-1
+
+    if l[s] < l[e]:
+        return 0
+
+    while s <= e:
+        m = s + (e-s)//2
+
+        if l[m] < l[m-1] and m != 0:
+            return m
+        if l[m] > l[e]:
+            s = m+1
+        else:
+            e = m-1
+
+nums = [4,5,6,7,0,1,2,3]
+print(number_times_sorted_array_rotated(nums))      
