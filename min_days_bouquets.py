5099f395e7d166a65e39814bbc7e0617550f32f5
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-04 23:51:23 +0530

    Day19

--- min_days_bouquets.py
+++ min_days_bouquets.py
@@ -4,27 +4,36 @@ def min_days_bouquets(l,k,bouquets):
     n = len(l)
     ans = -1
 
+    if bouquets * k > n:
+        return ans
+
     while s <= e:
         m = s + (e-s)//2
         
-        i = 0
         possible_bouquets = 0
-        while i <= n-k:
-            if max(l[i:i+k]) >= m:
-                possible_bouquets += 1
-                i += k
+        flowers = 0
+        for i in range(n):
+            if l[i] <= m:
+                flowers += 1
             else:
-                i += 1
+                flowers = 0
+
+            if flowers == k:
+                possible_bouquets += 1
+                flowers = 0
         
-        if possible_bouquets >= bouquets:
+            if possible_bouquets == bouquets:
+                break
+        
+        if possible_bouquets == bouquets:
             ans = m
-            s = m+1
-        else:
             e = m-1
+        else:
+            s = m+1
         
     return ans
 
-bloomDay = [1,10,3,10,2]
-m = 3
-k = 1
+bloomDay = [7,7,7,7,12,7,7]
+m = 2
+k = 3
 print(min_days_bouquets(bloomDay,k,m))
