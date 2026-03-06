9e6449d7f5508294effb35280bb8dedd5ed6aea9
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-03-06 22:26:40 +0530

    Day 21

--- kth_element_two_sorted_arrays.py
+++ kth_element_two_sorted_arrays.py
@@ -13,13 +13,14 @@ def kth_element_two_sorted_arrays(nums1,nums2,k):
 
         maxleft1 = float("-inf") if p1 == 0 else nums1[p1-1]
         minright1 = float("-inf") if p1 == m else nums1[p1]
-        maxleft2 = float("-inf") if p2 == 0 else nums2[p2-1]
-        minright2 = float("-inf") if p2 == m else nums2[p2]
+        maxleft2 = float("inf") if p2 == 0 else nums2[p2-1]
+        minright2 = float("inf") if p2 == n else nums2[p2]
 
         if maxleft1 <= minright2 and maxleft2 <= minright1:
             return max(maxleft1,maxleft2)
-        else:
+        elif maxleft1 > minright2:
             e = p1 - 1
+        else:
             s = p1 + 1
     
 
@@ -27,4 +28,3 @@ a = [2, 3, 6, 7, 9]
 b = [1, 4, 8, 10]
 k = 5  
 print(kth_element_two_sorted_arrays(a,b,k))
-
