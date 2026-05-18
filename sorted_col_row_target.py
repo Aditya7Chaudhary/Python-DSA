59264fb8d08e69b058b98a822d0da6fd11b4c599
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-05-18 10:47:23 +0530

    Day 23

--- sorted_col_row_target.py
+++ sorted_col_row_target.py
@@ -1,22 +1,18 @@
 def sorted_col_row_target(matrix,target):
-    i = len(matrix)-1
+    i = 0
     j = len(matrix[0])-1
 
     while i >= 0 and j >= 0 and i < len(matrix) and j < len(matrix[0]):
         if matrix[i][j] > target:
-                i -= 1
-                j -= 1
+            j -= 1
         elif matrix[i][j] < target:
-            if matrix[i+1][j] > matrix[i][j+1]:
-                i += 1
-            else:
-                j += 1
+            i += 1
         else:
-            return True                 
+            return True
 
     return False
 
 matrix = [[1,4,7,11,15],[2,5,8,12,19],[3,6,9,16,22],[10,13,14,17,24],[18,21,23,26,30]]
-target = 18
+target = 14
 
 print(sorted_col_row_target(matrix,target))
