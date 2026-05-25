70d8cc87bc6c4c0190e81f40e5ce9b0b81550888
Aditya7Chaudhary <adityachaudhary.eg@gmail.com>
2026-05-25 17:17:52 +0530

    Day 24

--- reverse_word_string.py
+++ reverse_word_string.py
@@ -1,15 +1,19 @@
 def reverse_word_string(s):
     i,e = len(s)-1,len(s)
+    j = e+1
     ans = ""
+    s += " "
 
     while i > 0:
         if s[i] == " ":
-            ans += s[i+1:e] + " "
+            ans += s[j:e+1]
             e = i
+        else:
+            j = i
         i -= 1
 
     ans += s[i:e]
     return ans
 
-s = "Welcome to the jungle"
+s = "      Welcome                           to             the      jungle      "
 print(reverse_word_string(s))
